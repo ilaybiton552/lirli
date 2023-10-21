edb82cc4fdc77b1df239d0fb7cad9155c9b67337
ilaybiton552 <ilaybitonminecraft@gmail.com>
2023-10-21 12:57:53 +0300

    Finished the project BirthdaySurprise TODO: Copy all needed files for birthday surprise (c++ file) Open the c++ exe program in Project_API Make sure everything workd outside debugger

--- SecondAnniversary_Lior/Birthday_Surprise/MainWindow.xaml.cs
+++ SecondAnniversary_Lior/Birthday_Surprise/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using AudioSwitcher.AudioApi.CoreAudio;
+using Project_API;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -10,10 +13,12 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using WpfAnimatedGif;
 
 namespace Birthday_Surprise
 {
@@ -22,26 +27,108 @@ namespace Birthday_Surprise
     /// </summary>
     public partial class MainWindow : Window
     {
-        private DispatcherTimer timer = new DispatcherTimer();
+        private Stopwatch stopwatch;
+        private bool playGifSound = true;
+        private bool playGif = true;
+        private MediaPlayer mediaPlayer;
+        private MediaPlayer player;
+        private double confettiTime = 6.5;
+        private bool mediaEnded = false;
+        private double width;
+        private double height;
+        private CoreAudioDevice defaultPlaybackDevice;
 
         public MainWindow()
         {
             InitializeComponent();
-            //if (DateTime.Now.Day != 31 || DateTime.Now.Month != 10)
-            //{
-            //    Close();
-            //}
+            if (DateTime.Now.Day != 31 || DateTime.Now.Month != 10)
+            {
+                Close();
+            }
 
-            timer.Interval = TimeSpan.FromSeconds(1);
-            timer.Tick += TimerTick;
-            timer.Start();
+            // get the screen width and height
+            width = SystemParameters.PrimaryScreenWidth;
+            height = SystemParameters.PrimaryScreenHeight;
+
+            // get the default playback devoid
+            defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
+
+            CompositionTarget.Rendering += CompositionTarget_Rendering;
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.Open(new Uri("confettisound.mp3", UriKind.Relative));
+            player = new MediaPlayer();
+            player.Open(new Uri("birthday.mp3", UriKind.Relative));
+            player.Play();
+            player.Volume = .25;
+            player.MediaEnded += Player_MediaEnded;
         }
 
-        private void TimerTick(object sender, EventArgs e)
+        private void Player_MediaEnded(object sender, EventArgs e)
         {
-            if (tx.FontSize < 200) tx.FontSize += 2;
-            else timer.Stop();
+            mediaEnded = true;
         }
 
+        private void CompositionTarget_Rendering(object sender, EventArgs e)
+        {
+            if (tx.Visibility == Visibility.Visible && tx.FontSize < 150)
+                tx.FontSize += 5;
+            if (playGifSound && stopwatch.ElapsedMilliseconds > 1000 * (confettiTime - .6))
+            {
+                playGifSound = false;
+                mediaPlayer.Play();
+            }
+            if (playGif && stopwatch.ElapsedMilliseconds > 1000 * confettiTime)
+            {
+                playGif = false;
+                ImageBehavior.SetAnimatedSource(gifRight, new BitmapImage(new Uri("/Images/confetti.gif", UriKind.Relative)));
+                ImageBehavior.SetAnimatedSource(gifLeft, new BitmapImage(new Uri("/Images/confetti.gif", UriKind.Relative)));
+                tx.Visibility = Visibility.Visible;
+            }
+            if (defaultPlaybackDevice.IsMuted) defaultPlaybackDevice.Mute(false);
+            if (defaultPlaybackDevice.Volume < 50) defaultPlaybackDevice.Volume = 50;
+        }
+
+        private void Gif_AnimationCompleted(object sender, RoutedEventArgs e)
+        {
+            CustomMessageBox customMessageBox = new CustomMessageBox("Well that's it lol\nJust thought it would be fun to trick you like that haha\nHappy Birthday Lior❤\n(find the close button)", "Happy Birthday");
+            customMessageBox.ShowDialog();
+            close.Visibility = Visibility.Visible;
+        }
+
+        private void Border_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Border border = sender as Border;
+            ColorAnimation animation = new ColorAnimation
+            {
+                To = (Color)ColorConverter.ConvertFromString("#1569C7"),
+                Duration = TimeSpan.FromSeconds(.2)
+            };
+            border.Background.BeginAnimation(SolidColorBrush.ColorProperty, animation);
+        }
+
+        private void Border_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Border border = sender as Border;
+            ColorAnimation animation = new ColorAnimation
+            {
+                To = (Color)ColorConverter.ConvertFromString("#50C878"),
+                Duration = TimeSpan.FromSeconds(.2)
+            };
+            border.Background.BeginAnimation(SolidColorBrush.ColorProperty, animation);
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!mediaEnded)
+            {
+                Border border = sender as Border;
+                Random rnd = new Random();
+                Canvas.SetLeft(border, rnd.Next((int)width - 200));
+                Canvas.SetTop(border, rnd.Next((int)height - 60));
+            }
+            else Close();
+        }
     }
 }
